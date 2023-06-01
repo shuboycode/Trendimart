bef29e956cccbe3333d8ac623977953ede8383a9
shubham <shubhamsher47@gmail.com>
2023-06-01 22:52:38 +0530

    setup the code base of this project.

---
+++ trendimart/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          this is trendimart.com
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          shop now
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
