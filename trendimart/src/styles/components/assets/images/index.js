3aeaecb45fec75b9b6aab44a929bc384337bd0fb
varsha <dharwalvarsha1999@gmail.com>
2023-06-27 18:37:42 +0530

    payment-page

--- trendimart/src/styles/components/assets/images/index.js
+++ trendimart/src/styles/components/assets/images/index.js
@@ -8,6 +8,13 @@ import DanimLadish from "./DanimLadish.svg";
 import DanimColor from "./DanimColor.svg";
 import Review from "./Review.svg";
 import ItemOne from "./ItemOne.svg";
+import Jacket from "./Jacket.svg";
+import Rupees from "./Rupees.svg";
+import Frame from "./Frame.svg";
+import Rup from "./Rup.svg";
+import Rupe from "./Rupe.svg";
 
 
-export{aboutus,logoTrendimart,t,logot,DanimImage, DanimLadish,DanimColor,Review,ItemOne};
+
+
+export{aboutus,logoTrendimart,t,logot,DanimImage, DanimLadish,DanimColor,Review,ItemOne, Jacket,Rupees,Frame,Rup,Rupe };
